d8d0651ba8db6585671a8c34c3e0fa3810d0ae3c
GhostDuck <94322246+Zanitas404@users.noreply.github.com>
2024-03-21 00:08:19 +0100

    Update datacaver.py

--- datacaver.py
+++ datacaver.py
@@ -1,45 +1,45 @@
 import pykd, sys, argparse, re
 
-prot_constants = [0x04]	# Protection constants to check if the memory region is executable
-running_addr = 0				# Used to skip the current code cave (recursion yay)
+prot_constants = [0x04]																			# Protection constants to check if the memory region is executable
+running_addr = 0																				# Used to skip the current code cave (recursion yay)
 
 
 def analyze_cave(start_addr):
 	global running_addr
-	flag = True					                                                              # Used to find the end of the data cave
-	counter = 0					                                                              # Used to move through the memory region
+	flag = True													# Used to find the end of the data cave
+	counter = 0													# Used to move through the memory region
 	while flag:
-		out = pykd.dbgCommand('dd ({0} + 4 * 0n{1}) L4'.format(start_addr, counter))		# Display the memory region
-		counter = counter + 1 																                          # Move forward in the memory region
-		if not '00000000 00000000 00000000 00000000' in out:								            # Checks if the region stores data
-			flag = False																	                                # If it does, the flag will be inverted and the loop ends
-			out = pykd.dbgCommand('dd ({0} + 4 * 0n{1}) L4'.format(start_addr, counter - 1))	# Obtain the end of the code cave
-			running_addr = int(out.split('  ')[0], 16)										                # Obtain the end addr of the code cave in order to move on to the next one
+		out = pykd.dbgCommand('dd ({0} + 4 * 0n{1}) L4'.format(start_addr, counter))				# Display the memory region
+		counter = counter + 1											# Move forward in the memory region
+		if not '00000000 00000000 00000000 00000000' in out:							# Checks if the region stores data
+			flag = False																	    # If it does, the flag will be inverted and the loop ends
+			out = pykd.dbgCommand('dd ({0} + 4 * 0n{1}) L4'.format(start_addr, counter - 1))		# Obtain the end of the code cave
+			running_addr = int(out.split('  ')[0], 16)							# Obtain the end addr of the code cave in order to move on to the next one
 	code_cave_size = (counter + 1 ) * 4														                    # Calculate the data cave size using the counter
 	pykd.dprintln('|\t{0}\t|\t\t{1}\t\t|'.format(start_addr, code_cave_size))
 
 
 def vprot(addr):
 	out = pykd.dbgCommand('!vprot {0}'.format(addr))
-	out = out.split('Protect:           ')[1].split(' ')[0]			                      # Obtain the hex value of the protection value
+	out = out.split('Protect:           ')[1].split(' ')[0]								# Obtain the hex value of the protection value
 	try:
-		out = int(out, 16)											                                        # Convert the string to int base 16
+		out = int(out, 16)											# Convert the string to int base 16
 	except Exception as e:
 		pykd.dprintln(e)
 	
-	if out in prot_constants:										                                      # If the memory region is executable -> return the address
+	if out in prot_constants:											# If the memory region is executable -> return the address
 		analyze_cave(addr)
 
 
 def analyze(input):
 	out_arr = input.split('\n')
 	for x in out_arr:
-		if '00000000 00000000 00000000 00000000' in x:				                          # Check if the region is empty
-			vprot(x.split('  ')[0])							                                          # If it is, move to vprot() in order to analyze it further		
+		if '00000000 00000000 00000000 00000000' in x:								# Check if the region is empty
+			vprot(x.split('  ')[0])										# If it is, move to vprot() in order to analyze it further		
 
 			
-def looper(start, end):												                                      # This method is used in order to skip a data cave if one is found
-	global running_addr												                                        # So that the script does not analyze the same cave multiple times
+def looper(start, end):													# This method is used in order to skip a data cave if one is found
+	global running_addr												# So that the script does not analyze the same cave multiple times
 	for address in range(start, end, 0xA):
 		if running_addr != 0:
 			tmp = running_addr
@@ -52,17 +52,17 @@ def looper(start, end):												                                      # This 
 
 def print_welcome_message():
 	pykd.dprintln('''
+
+
+    ____          __          ______                         
+   / __ \ ____ _ / /_ ____ _ / ____/____ _ _   __ ___   _____
+  / / / // __ `// __// __ `// /    / __ `/| | / // _ \ / ___/
+ / /_/ // /_/ // /_ / /_/ // /___ / /_/ / | |/ //  __// /    
+/_____/ \__,_/ \__/ \__,_/ \____/ \__,_/  |___/ \___//_/     
+                                                              
                                                                               
-                                                                              
-██████╗  █████╗ ████████╗ █████╗      ██████╗ █████╗ ██╗   ██╗███████╗██████╗ 
-██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗    ██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
-██║  ██║███████║   ██║   ███████║    ██║     ███████║██║   ██║█████╗  ██████╔╝
-██║  ██║██╔══██║   ██║   ██╔══██║    ██║     ██╔══██║╚██╗ ██╔╝██╔══╝  ██╔══██╗
-██████╔╝██║  ██║   ██║   ██║  ██║    ╚██████╗██║  ██║ ╚████╔╝ ███████╗██║  ██║
-╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝     ╚═════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
-                                                                              
- 											written by Ghostduck
-                      inspired by nop
+ 							written by GhostDuck
+							inspired by nop
 		''')
 
 
